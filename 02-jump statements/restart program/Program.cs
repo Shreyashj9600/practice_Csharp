d7b01b515b46d9bc1ac6b84de2be9e7e6add6837
Shreyashj9600 <connectshreyashj96@gmail.com>
2024-07-07 02:15:59 +0530

    jump statements

---
+++ 02-jump statements/restart program/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace restart_program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 10; i++) 
+            {
+                if (i == 0)
+                {
+                   goto stop;
+                }
+                else if (i == 3)
+                {
+                    goto stop;
+                }
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("loop end");
+
+            stop:
+            Console.WriteLine("program exits");
+
+            Console.ReadLine();
+        }
+    }
+}
