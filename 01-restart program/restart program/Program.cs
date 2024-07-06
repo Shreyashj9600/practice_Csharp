007ef9a9cecbef14964d54c40188cb28f483713a
Shreyashj9600 <connectshreyashj96@gmail.com>
2024-07-07 01:50:47 +0530

    reset c-sharp program

---
+++ 01-restart program/restart program/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace restart_program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string confirm;
+            do
+            {
+                Console.WriteLine("Enter first number");
+                int num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Enter second number");
+                int num2 = int.Parse(Console.ReadLine());
+                int add = num1 + num2;
+                Console.WriteLine("Addation result is : " + add);
+
+                Console.WriteLine("start from one more time yes / no");
+                confirm = Console.ReadLine().ToLower();
+
+            } while (confirm == "yes");
+
+            Console.ReadLine();
+        }
+    }
+}
