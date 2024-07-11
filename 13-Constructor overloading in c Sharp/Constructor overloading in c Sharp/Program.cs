0dcf76a8a146e57541c9b1809780fcba072cb167
Shreyashj9600 <connectshreyashj96@gmail.com>
2024-07-11 20:29:01 +0530

    coustructor overloading

---
+++ 13-Constructor overloading in c Sharp/Constructor overloading in c Sharp/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Constructor_overloading_in_c_Sharp
+{
+    internal class Program
+    {
+        public Program()
+        {
+            Console.WriteLine("this is a first constructor");
+        }
+        public Program(int a, int b)
+        {
+            Console.WriteLine("this is a second constructor" + (a + b) );
+        }
+        public Program(string name)
+        {
+            Console.WriteLine("this is a third  constructor : " + name);
+        }
+        public Program(string fullname, int age)
+        {
+            Console.WriteLine("this is a fourth " + fullname, age);
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program(2,2);
+        }
+    }
+}
