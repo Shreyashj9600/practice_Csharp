af20dacfc7e71f1330373bab1093f5cfd823638a
Shreyashj9600 <connectshreyashj96@gmail.com>
2024-07-11 16:34:51 +0530

    Constructor in c sharp

---
+++ 11-Constructors in C sharp/Constructors in C sharp/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace Constructors_in_C_sharp
+{
+    internal class Program
+    {
+        public Program() 
+        {
+            Console.WriteLine("constructor innvokes !!");
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+        }
+    }
+}
