45a892cbe4eb98dacdf95b128a437cbaf93b1584
Shreyashj9600 <connectshreyashj96@gmail.com>
2024-07-13 01:25:07 +0530

    PRIVATE CONSTRUCTOR

---
+++ 17-PRIVATE CONSTRUCTOR/PRIVATE CONSTRUCTOR/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace PRIVATE_CONSTRUCTOR
+{
+    class Example
+    {
+        public Example() { }
+        private Example() 
+        {
+            
+        }
+        public static void getTime()
+        {
+            Console.WriteLine(DateTime.Now);
+        }
+    }
+
+    //class Example2 : Example
+    //{
+        
+    //}
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Example e = new Example();
+            Example.getTime(); 
+        }
+    }
+}
