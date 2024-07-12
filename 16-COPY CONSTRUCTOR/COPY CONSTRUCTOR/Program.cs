621183ca6a7e30654b100e17c0a9f96131241d97
Shreyashj9600 <connectshreyashj96@gmail.com>
2024-07-12 21:56:55 +0530

    copy constructor

---
+++ 16-COPY CONSTRUCTOR/COPY CONSTRUCTOR/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace COPY_CONSTRUCTOR
+{
+    class Example
+    {
+        public string name;
+        public int age;
+
+        public Example(string name, int age)
+        { 
+            this.name = name;
+            this.age = age;
+        }
+
+        public Example(Example e)
+        {
+            this.name = e.name;
+            this.age = e.age;
+        }
+        public void getData()
+        {
+            Console.WriteLine("your name is : {0}" , name);
+            Console.WriteLine("your name is : {0}" , age);
+        }
+    }
+ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Example obj = new Example("rohit", 10);
+            obj.getData();
+
+            Example obj1 = new Example(obj);
+            obj1.getData();
+
+            Console.ReadLine();
+        }
+    }
+}
