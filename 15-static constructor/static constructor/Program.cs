8698a6a4bbfa3a57bb05905ca636fb9e011219b4
Shreyashj9600 <connectshreyashj96@gmail.com>
2024-07-12 18:23:12 +0530

    static counstroctor

---
+++ 15-static constructor/static constructor/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace static_constructor
+{
+    class Person
+    {
+        public static string name = "rohit";
+        public static int age = 10;
+
+        static Person()
+        {
+            Console.WriteLine("staric constructor invoke !!");
+        }
+
+        public Person() 
+        {
+            Console.WriteLine("defaul constructor invoke !!");
+        }
+        public static void getDetails()
+        {
+            Console.WriteLine("your name is " + name );
+        }
+    }
+  
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          Person rohit = new Person();
+            Person.getDetails();
+
+          
+        }
+    }
+}
